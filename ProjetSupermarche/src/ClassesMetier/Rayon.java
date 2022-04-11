8f1a4fc8d40b2f10968ad99bb2b16c2395e2253b
alexandre lyfoung <alexandrelyfoung@hotmail.fr>
2022-04-11 12:19:41 +0200

    Create Rayon Constructor + Add attribut to Rayon object + Add get function (getIdRayon, getNomRayon, getLesTravailler)

--- ProjetSupermarche/src/ClassesMetier/Rayon.java
+++ ProjetSupermarche/src/ClassesMetier/Rayon.java
@@ -4,10 +4,36 @@
  */
 package ClassesMetier;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Alex
  */
 public class Rayon {
+    private int idRayon;
+    private String nomRayon;
+    private ArrayList<Travailler> lesTravailler;
     
+    public Rayon(int unId, String unNom)
+    {
+        this.idRayon = unId;
+        this.nomRayon = unNom;
+        this.lesTravailler = new ArrayList<Travailler>();
+    }
+    
+    public int getIdRayon()
+    {
+        return this.idRayon;
+    }
+    
+    public String getNomRayon()
+    {
+        return this.nomRayon;
+    }
+    
+    public ArrayList<Travailler> getLesTravailler()
+    {
+        return this.lesTravailler;
+    }
 }
